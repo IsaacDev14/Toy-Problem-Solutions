c01a03eb838f5e209020af52fdfd5ca0ec1a2ab1
IsaacDev14 <mwitiisaac14@gmail.com>
2025-03-09 23:14:43 +0300

    create speed deteccctor program

---
+++ SpeedDetector.js
@@ -0,0 +1,21 @@
+function checkSpeed() {
+    const speed = parseInt(prompt("Enter the speed of the car:"));
+    speed = parseInt(speed);
+
+    const speedLimit = 70;
+    const kmOver = 5;
+
+    
+    if (speed <= speedLimit) {
+        console.log("Ok");
+    } else {
+        const demeritPoints = Math.floor((speed - speedLimit) / kmOver);
+        if (demeritPoints > 12) {
+            console.log("License suspended");
+        } else {
+            console.log(`Points: ${demeritPoints}`);
+        }
+    }
+}
+
+checkSpeed();
